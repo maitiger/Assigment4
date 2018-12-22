3d48fe876ac03a832bf00c970f15ac84b1b9e145
Anhcuongsuper <43609383+Anhcuongsuper@users.noreply.github.com>
2018-12-22 12:21:25 +0700

    vua tai entity

---
+++ ClientEAP/ClientEAP/entity/login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientEAP.entity
+{
+    class login
+    {
+        public int abc { get; set; }
+    }
+}
